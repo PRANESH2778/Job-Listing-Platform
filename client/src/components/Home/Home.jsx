97c2f4f4846fb3482aade010f09f07fb7ea3c831
Pranesh S <praneshpalaniappa27@gmail.com>
2024-02-16 19:11:25 +0530

    modified

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -36,7 +36,7 @@ export default function Home() {
   },[search])
   const handleSearch = (e) => {
     setSearch(e.target.value);
-    if(search.length === 0 ){
+    if(e.target.value.length === 0 ){
       showAllJobs()
     }
   };
@@ -88,9 +88,9 @@ export default function Home() {
       if(!search){
         showAllJobs()
       }else{
-        console.log(reqPayload);
+        // console.log(reqPayload);
         const result = await getAllJobs(reqPayload);
-        console.log(result);
+        // console.log(result);
         setJobDetails(result);
         setIsLoading(false)
         if(result.length === 0){
